4e501fe0d79fd8bda612f652ef91e9303d5e9359
kboy101222 <george.kyle87@gmail.com>
2019-11-20 00:34:11 -0600

    Added a custom token template for testing purposes

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var cardTemplate =
+'<div class="card" style="width:18rem;">'+
+'<div class="card-body">'+
+'<h5 class="card-title">New Token</h5>'+
+'<small>Token Name</small>'+
+'<input type="text" class="form-control">'
